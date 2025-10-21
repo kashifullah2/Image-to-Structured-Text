7ae08677eba3a45e60158c69cb16d750cbc494a2
Kashif Ullah <Kashifullah919@gmail.com>
2025-10-21 20:12:09 +0500

    Rearranged project structure and removed old files

--- app/main.py
+++ app/main.py
@@ -2,13 +2,13 @@ from fastapi import FastAPI, UploadFile, Request, Form, File
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from services.extractor import Extractor
-from services.text_fixer import TextFixer
+from app.services.extractor import Extractor
+from app.services.text_fixer import TextFixer
 import json
+
 
 app = FastAPI()
 
-# Static and templates
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 templates = Jinja2Templates(directory="app/templates")
 
@@ -19,13 +19,8 @@ def home(request: Request):
 
 
 @app.post("/add", response_class=HTMLResponse)
-async def upload_files(
-    request: Request,
-    files: list[UploadFile] = File(...),
-    fields: str = Form(...)
-):
+async def upload_files(request: Request, files: list[UploadFile] = File(...), fields: str = Form(...)):
     results = []
-
     for file in files:
         extractor = Extractor(file)
         image_to_text = await extractor.extract_text()
@@ -35,12 +30,6 @@ async def upload_files(
         data = json.loads(final)
         results.append(data)
 
-    # Render results.html
     return templates.TemplateResponse(
         "result.html", {"request": request, "results": results}
     )
-
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", reload=True)
